6f58afb776a40bf82400bd51e98ff929946b771e
MathieuGAUBAL <b_jayson@hotmail.com>
2021-11-23 11:45:54 +0100

    Updates

---
+++ precache-manifest.a5a621f4ddbab494981ceeece0e90cac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9d4690e93c4dc2d4ed6a8e5335ef5d",
+    "url": "/lapsteelator/index.html"
+  },
+  {
+    "revision": "7710a58daadb6807a6d9",
+    "url": "/lapsteelator/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "216c86f08e4ce89058f7",
+    "url": "/lapsteelator/static/css/main.7a2f553b.chunk.css"
+  },
+  {
+    "revision": "7710a58daadb6807a6d9",
+    "url": "/lapsteelator/static/js/2.cbc7fe98.chunk.js"
+  },
+  {
+    "revision": "8f409196425b9cef9777aa9cd0e133da",
+    "url": "/lapsteelator/static/js/2.cbc7fe98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "216c86f08e4ce89058f7",
+    "url": "/lapsteelator/static/js/main.8e4c3794.chunk.js"
+  },
+  {
+    "revision": "4ea5f56581a7c0d2c58f",
+    "url": "/lapsteelator/static/js/runtime-main.eaecf887.js"
+  }
+]);
